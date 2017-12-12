3012cf673d47cb3893a7ac4316970cbb422429c6
riba0007 <riba0007@algonquinlive.com>
2017-12-11 20:37:54 -0500

    first commit

---
+++ src/actions.js
@@ -0,0 +1,26 @@
+export const TOGGLE_STATUS = "TOGGLE_STATUS";
+
+export const ADD_TODO_CHANGED = "ADD_TODO_CHAGED";
+
+export const SAVE_NEW_TODO = "SAVE_NEW_TODO";
+
+
+export function toggleStatus(id){
+    return {
+        type: TOGGLE_STATUS,
+        id: id
+    };
+}
+
+export function addTodoChanged(value){
+    return {
+        type: ADD_TODO_CHANGED,
+        value: value
+    };
+}
+
+export function saveNewTodo(){
+    return {
+        type: SAVE_NEW_TODO  
+    };
+}
